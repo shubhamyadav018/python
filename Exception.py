3d5a81c47479193a541bdeb2186b410a45a88b8a
shubhamyadav018 <167082096+shubhamyadav018@users.noreply.github.com>
2024-04-28 10:14:14 +0530

    Update Exception.py

--- Exception.py
+++ Exception.py
@@ -15,3 +15,25 @@ finally:
 print("outside the try block")
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+print("starting line")
+a=[11,22,33]
+
+try:
+    #a=10 / 5
+    #print(a[5])
+    print(a)
+except zeroDivisionError:
+        print("exception raised due to zero division error")
+except IndexError:
+        print("exception raised due to index out of range")
+except NameError:
+    print("exception raised due to undefined variable")
+except:
+    print("same exception raised")
+else:
+    print("NO exception raised . everything worked perfrectly")
+finally:
+    print("this is a final block")
+print("outside the try block")
