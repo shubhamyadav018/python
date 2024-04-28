b857416f952cab561b27f388563cad81cae1b990
shubhamyadav018 <167082096+shubhamyadav018@users.noreply.github.com>
2024-04-28 10:16:12 +0530

    Create student details 2.py

---
+++ student details 2.py
@@ -0,0 +1,13 @@
+class Box:
+    def __init__(self,nameofstudent,rollno):
+        self.name=nameofstudent
+        self.rollno=rollno
+
+obj1=Box("shiv",45)
+obj2=Box("raj",23)
+
+print(obj1.name)
+print(obj1.rollno)
+
+print(obj2.name)
+print(obj2.rollno)
