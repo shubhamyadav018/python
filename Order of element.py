44d32863d2ae232fd7e27c3d1caca86e93819a5a
shubhamyadav018 <167082096+shubhamyadav018@users.noreply.github.com>
2024-04-28 10:03:56 +0530

    Create Order of element.py

---
+++ Order of element.py
@@ -0,0 +1,14 @@
+word=input()
+t=print(word[5:10])
+t=print(word[14:20])
+t=print(word[21:25])
+t=print(word[26:29])
+t=print(word[0:4])
+t=print(word[5:16])
+t=print(word[17:29])
+
+
+
+____________________________________
+
+t=print(word[15:9:-1])   ||||||||| for reverse the order 
