1ea90a9c67097900a0812e37ded5d8dbbf474943
shubhamyadav018 <167082096+shubhamyadav018@users.noreply.github.com>
2024-04-28 10:05:47 +0530

    Create Storing word.py

---
+++ Storing word.py
@@ -0,0 +1,36 @@
+word = input()
+print(word)
+
+
+
+store = dict()
+print(store)
+
+store['P'] = 23
+store['Q'] = 'abcd'
+store['W'] = 22
+store[12] = 11
+store["hello"] = 222
+print(store)
+
+allkeys = store.keys()
+print(allkeys)
+
+
+
+#for-each loop
+for ele in allkeys:
+
+    print(ele,store[ele])
+
+
+#   for ch in  "abcdef":
+#        print(ch)
+
+#   for val in [13,23,22,45,78,222,11,34]:
+#        print(val)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+input==pythonnn
+
