06c471b40884a8c3415611c84cc865cf8f810a8a
shubhamyadav018 <167082096+shubhamyadav018@users.noreply.github.com>
2024-04-28 10:09:32 +0530

    Create Line excecution.py

---
+++ Line excecution.py
@@ -0,0 +1,11 @@
+
+print("last line getting executed")
+
+i = 23
+while i < 30:
+    print("statement-1")
+    print("statement-2")
+    print("statement-3")
+    i = i + 1
+print("first line getting executed ")
+print("second line getting  executed")
